765dff9a7af20c7597bc279683d034e4a1b564af
JeremiahID <jeremiahefe.je@gmail.com>
2024-02-25 12:18:47 +0100

    images display done

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-
+// this is the scripting for the first dice
 var randomNumberOne = Math.floor(Math.random() * 6) + 1;
 
 var randonImageGenarator = "./images/dice" + randomNumberOne + ".png";
 
-var setAttributesOne = document.querySelectorAll(img)[0].setAttribute('src' , randonImageGenarator);
+var setAttributesOne = document.querySelectorAll("img")[0].setAttribute("src" , randonImageGenarator);
+
+// this is the scripting for the second dice
+var randomNumberTwo = Math.floor(Math.random() * 6) + 1;
+
+var randonImageGenaratorTwo = "./images/dice" + randomNumberTwo + ".png";
+
+var setAttributesTwo = document.querySelectorAll("img")[1].setAttribute("src" , randonImageGenaratorTwo);
+
+
+if(randomNumberOne > randomNumberTwo){
+    document.querySelector('h1').innerHTML = "PLAYER ONE WINS"
+} 
+else if (randomNumberTwo > randomNumberOne){
+    document.querySelector('h1').innerHTML = "PLAYER ONE WINS"
+} else {
+    
+}
